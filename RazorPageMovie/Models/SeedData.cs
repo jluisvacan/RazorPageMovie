c588e421e3cc1ebb5160f4c8e1914325292a9aeb
Luis Vaca <jluis.vacan64@gmail.com>
2024-03-01 12:37:30 -0600

    Add project files.

---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+
+namespace RazorPageMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPageMovieContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentException("Null RazorPagesMovieContext");
+                }
+
+                //Para mirar cualquier pelicula
+                if (context.Movie.Any()) {
+                    return; //DB muestra todo lo que contiene la clase
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry met Sally",
+                        ReleaseDate = DateTime.Parse("1999-02-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-03-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "G"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-02-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "G"
+                     },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-04-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "NA"
+                     }
+                    );
+                context.SaveChanges();
+            }
+        }
+        
+    
+    }
+}
